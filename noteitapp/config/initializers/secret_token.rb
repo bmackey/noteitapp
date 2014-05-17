5fa7249b638a019ea1d6bb4cb088b00e720a78ba
Brid Mackey <brid.mackey@gmail.com>
2014-05-17 13:37:58 +0100

    initalize repo

---
+++ noteitapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Noteitapp::Application.config.secret_token = '8fadaf29b5ce3c89c6c8cdbac1761fdc997d1e1c5c2ec3c7e7f3c5a5472608f6d5aafa81e84fd13d548f6fa39785f1517034bfa24a629828f5e050943b04754f'
